ae44921a8a2bd011ed54b6d481b19ab3102b6ac5
Andy Hoffman <antibland@gmail.com>
2017-03-26 14:26:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Angular Tunes
+
+An Angular 2 application that displays artists and albums.
+Used to show how to use the Router to navigate between different components.
+Sample data courtesy of [MusicBrainz](https://musicbrainz.org/).
---
+++ src/app/login/login-form.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from './login.service';
+
+@Component({
+  template: `
+    <h1>Login</h1>
+    <div>
+      <input type="text" [(ngModel)]="user" placeholder="User">
+      <input type="password" [(ngModel)]="password" placeholder="Password">
+      <button (click)="login()" class="btn btn-primary">Login</button>
+    </div>
+  `
+})
+export class LoginFormComponent {
+
+  user = '';
+  password = '';
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private loginService: LoginService) { }
+
+  login() {
+    if (this.loginService.login(this.user, this.password)) {
+      const destination = this.route.snapshot.queryParams['destination'] || '/';
+      this.router.navigateByUrl(destination)
+    }
+  }
+
+}
